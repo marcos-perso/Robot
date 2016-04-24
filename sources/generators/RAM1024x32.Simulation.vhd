2e46787f2ac74beaabd2ffcc9fcc3564203c11aa
Marcos Martinez <marcos.martinez@gmail.com>
2016-04-24 20:11:25 +0200

    Simualtion running (not verified)

--- sources/generators/RAM1024x32.Simulation.vhd
+++ sources/generators/RAM1024x32.Simulation.vhd
@@ -10,8 +10,8 @@ use work.UtilsPackage.all;
 architecture Simulation of RAM1024x32 is
 
 
-constant content_file_in  : string := "/home/mmartinez/Projects/Robot/design/test/system/test1/RAM1024x32.in";
-constant content_file_out : string := "/home/mmartinez/Projects/Robot/design/test/system/test1/RAM1024x32.out";
+constant content_file_in  : string := "/home/mmarttinez/Projects/Perso/Robot/design/test/system/test1/RAM1024x32.in";
+constant content_file_out : string := "/home/mmarttinez/Projects/Perso/Robot/design/test/system/test1/RAM1024x32.out";
 
 shared variable ram : ram_type_1024x32 := init_rom_1024x32(content_file_in,content_file_out);
 
